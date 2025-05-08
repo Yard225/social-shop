30ceab0cf3af360672ad2099f627237a80c0fd92
Yard225 <103653539+Yard225@users.noreply.github.com>
2025-05-08 22:57:03 +0000

    add jest config

--- src/services/ordering-svc/src/ports/repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Order } from '../core/aggregates/order.aggregate';
-
-export interface IOrderRepository {
-  findById(id: string): Promise<Order | null>;
-  create(order: Order): Promise<void>;
-}
--- src/services/ordering-svc/src/usecase/add-item-to-cart.usecase.ts
+++ src/services/ordering-svc/src/usecase/add-item-to-cart.usecase.ts
@@ -1,20 +1,3 @@
-import { ProductSKU } from '../core/value-objects/product-sku.vo';
-import { IOrderRepository } from '../ports/repository.interface';
-
-type Request = {
-  orderId: string;
-  productId: ProductSKU | undefined;
-  qty: number;
-};
-
-type Response = void;
-
 export class AddItemToCartUseCase {
-  constructor(private readonly repository: IOrderRepository) {}
-  async execute(request: Request): Promise<Response> {
-    const order = await this.repository.findById(request.orderId);
-
-    if (!order) {
-    }
-  }
+  constructor() {}
 }
